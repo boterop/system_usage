d0be09feb8325811481e7f1df5c514971f509a38
Santiago Botero <98826652+boterop@users.noreply.github.com>
2023-07-16 20:00:58 -0500

    Add CORS

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from lib.system import System
 from flask import Flask
+from flask_cors import CORS, cross_origin
 from waitress import serve
 
 app = Flask(__name__)
+CORS(app)
 
 
 @app.get('/system-usage')
